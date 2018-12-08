e098750583f05c8fb738719ccceeb75c29021ed6
Ivan Fraixedes <ivan@fraixed.es>
2018-12-08 11:46:49 +0100

    payment: Define the type for sorting payments
    Define the type for specifying how a list of payments must be sorted.

---
+++ payment/sort.go
@@ -0,0 +1,41 @@
+package payment
+
+// SortDir is the type which represents the direction when sorting values.
+type SortDir uint8
+
+// The list of valid SortDir values
+const (
+	SortUnspecified SortDir = iota
+	SortAscending
+	SortDescending
+)
+
+// Sort specifies the allowed fields for sorting a list of payments.
+// The payment's fields which aren't present aren't allowed to be used for
+// sorting.
+type Sort struct {
+	Type           SortDir
+	ID             SortDir
+	Version        SortDir
+	OrganisationID SortDir
+	Attributes     SortAttributes
+}
+
+// SortAttributes is the type of the Attributes field of the Sort type.
+type SortAttributes struct {
+	Amount             SortDir
+	Currency           SortDir
+	Reference          SortDir
+	EndToEndReference  SortDir
+	NumericReference   SortDir
+	PaymentID          SortDir
+	ProcessingDate     SortDir
+	ChargesInformation SortChargesInformation
+}
+
+// SortChargesInformation is the type of the ChargesInformation field of the
+// SortAttributes type.
+type SortChargesInformation struct {
+	SenderChargesAmount   SortDir
+	ReceiverChargesAmount SortDir
+}
